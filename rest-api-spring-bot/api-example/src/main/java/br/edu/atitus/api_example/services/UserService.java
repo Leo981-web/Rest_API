4390a190391978568fdd1d17fa0fd38aec981950
Leo981-web <leozancanaro@outlook.com.br>
2025-11-20 21:58:28 -0300

    Correções de bugs

--- rest-api-spring-bot/api-example/src/main/java/br/edu/atitus/api_example/services/UserService.java
+++ rest-api-spring-bot/api-example/src/main/java/br/edu/atitus/api_example/services/UserService.java
@@ -16,7 +16,8 @@ public class UserService implements UserDetailsService {
 	private final UserRepository repository;
 	private final PasswordEncoder enconder;
 	
-	private final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}(\\\\.[a-zA-Z]{2,})*$";
+	private final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,}$";
+	
 	private static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$";
 	
 	public UserService(UserRepository repository, PasswordEncoder enconder) {
@@ -26,39 +27,40 @@ public class UserService implements UserDetailsService {
 	}
 
 	public UserEntity save(UserEntity user) throws Exception {
-        if (user == null)
-            throw new Exception("Objeto Nulo");
+        	
+		    if (user == null)
+		        throw new Exception("Objeto Nulo");
 
-        if (user.getName() == null || user.getName().isEmpty())
-            throw new Exception("Nome inválido");
-        user.setName(user.getName().trim());
-		
-        if(user.getEmail() == null || !user.getEmail().matches(EMAIL_REGEX))
-        	throw new Exception("E-mail inválido. O formato deve ser 'usuario@dominio.com'");
-        user.setEmail(user.getEmail().trim());
-        
-        if (user.getPassword() == null || !user.getPassword().matches(PASSWORD_REGEX))
-            throw new Exception("Senha inválida. A senha deve ter no mínimo 8 caracteres, "
-                    + "conter pelo menos uma letra maiúscula, uma minúscula e um número.");
-        user.setPassword(enconder.encode(user.getPassword()));
-        
-        if (repository.existsByEmail(user.getEmail()))
-            throw new Exception("Já existe usuário cadastrado com este e-mail");
-        
-        if(user.getPhoneNumber() == null || !user.getPhoneNumber().matches("^\\(?[1-9]{2}\\)? ?(?:[2-8]|9[0-9])[0-9]{3}\\-?[0-9]{4}$"))
-        	throw new Exception("Telefone inválido. Use o formato (XX)9XXXX-XXXX ou similar");
-        
-        if (user.getPhoneNumber() == null || user.getPhoneNumber().isEmpty())
-            throw new Exception("Número de telefone inválido. Campo obrigatório.");
-        
-        if (user.getBirthDate() == null) 
-            throw new Exception("Data de nascimento inválida. Campo obrigatório.");
-        
-        if (user.getBirthDate() == null)
-        	throw new Exception("Data de nascimento obrigatória");
-        
-        if (user.getBirthDate().isAfter(java.time.LocalDate.now().minusYears(10))) 
-            throw new Exception("Usuário deve ter no mínimo 10 anos de idade");
+		    if (user.getName() == null || user.getName().isEmpty())
+		        throw new Exception("Nome inválido");
+		    
+		    user.setName(user.getName().trim());
+		   
+		    if (user.getEmail() == null) {
+		        throw new Exception("E-mail obrigatório");
+		    }
+		    
+		    user.setEmail(user.getEmail().trim());
+
+		    if(!user.getEmail().matches(EMAIL_REGEX))
+		        throw new Exception("E-mail inválido. O formato deve ser 'usuario@dominio.com'");
+		   
+		    if (repository.existsByEmail(user.getEmail()))
+		        throw new Exception("Já existe usuário cadastrado com este e-mail");
+
+		    if (user.getPassword() == null || !user.getPassword().matches(PASSWORD_REGEX))
+		        throw new Exception("Senha inválida. A senha deve ter no mínimo 8 caracteres...");
+		    
+		    user.setPassword(enconder.encode(user.getPassword()));
+		   
+		    if(user.getPhoneNumber() == null || !user.getPhoneNumber().matches("^\\(?[1-9]{2}\\)? ?(?:[2-8]|9[0-9])[0-9]{3}\\-?[0-9]{4}$"))
+		        throw new Exception("Telefone inválido. Use o formato (XX)9XXXX-XXXX ou similar");
+		    
+		    if (user.getBirthDate() == null) 
+		        throw new Exception("Data de nascimento obrigatória.");
+		    
+		    if (user.getBirthDate().isAfter(java.time.LocalDate.now().minusYears(10))) 
+		        throw new Exception("Usuário deve ter no mínimo 10 anos de idade");
         
         repository.save(user);
         
